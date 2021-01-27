f6da156755cc6a33974a70f1df17f6ac1f14a438
Moe <hayesm14@gmail.com>
2021-01-27 09:45:28 -0500

    first

---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const knex = require('knex')
+const app = express();
+
+const db = knex({
+    client: 'pg',
+    connection: {
+        host: '127.0.0.1',
+        user: 'postgres',
+        password: 'nOMAD92*',
+        database: 'acc'
+    }
+
+});
+
+
+
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
+
+const trackList = {
+    'Barcelona': 'barcelona',
+    'Brands Hatch': 'brandshatch',
+    'Hungaroring': 'hungaroring',
+    'Kyalami': 'kyalami',
+    'Laguna Seca': 'lagunaseca',
+    'Misano': 'misano',
+    'Monza': 'monza',
+    'Mount Panorama': 'mountpanorama',
+    'Nurburgring': 'nurburgring',
+    'Paul Ricard': 'paulricard',
+    'Silverstone': 'silverstone',
+    'Spa Francorchamps': 'spa',
+    'Suzuka': 'suzuka',
+    'Zandvoort': 'zandvoort',
+    'Zolder': 'zolder',
+    
+}
+
+app.post('/post', (req, res) => {
+    for (const [key, value] of Object.entries(trackList)) {
+        if (req.body.track === key) {
+            db.select('car', value).from('liters').where({ car: req.body.car })
+                .then(data => {
+                    const obj = data[0];
+                    const obj2 = Object.values(obj);
+                    res.json(obj2[1]);
+                    console.log(obj2);
+                })
+                .catch(err => res.json(err))
+        }
+    }
+});
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(process.env.PORT);
